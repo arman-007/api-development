74185cd4482e4880472153f958b8631b1ec8d32a
arman-007 <armanhosen9@gmail.com>
2025-07-28 19:56:21 +0600

    model schema defined, url and settings configured

--- items/models.py
+++ items/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(
+        max_length=100,
+        validators=[MaxValueValidator(100)],
+    )
+    desription = models.TextField(
+        max_length=1000,
+        validators=[MaxValueValidator(1000)],
+        blank=True,
+        null=True
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(0.01)],
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.name} - ${self.price:.2f}"
